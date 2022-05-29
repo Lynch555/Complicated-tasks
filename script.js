699696b02cd7c133f088bfb85267740bbe6c7537
Sergei Vishnevsky <vishneuski1983@mail.ru>
2022-05-29 19:02:28 +0300

    усложненное задание01

---
+++ script.js
@@ -0,0 +1,13 @@
+let num = 266219,
+    rezult,
+    rezult2;
+
+rezult = 2 * 6 * 6 * 2 * 1 * 9;
+console.log(rezult);
+
+rezult2 = rezult ** 3;
+console.log(rezult2);
+
+let digit1 = rezult2.toString()[0];
+let digit2 = rezult2.toString()[1];
+console.log(digit1, digit2);
